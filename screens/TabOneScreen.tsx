81b2f0ee733b2a0ea5f7da35fd1b56501b340c67
ouzsrcm <oguzhan.saricam@icloud.com>
2021-09-07 09:56:59 +0300

    video

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,20 +1,35 @@
-import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, Dimensions, Button } from 'react-native';
 
-import {Audio, Video} from 'expo-av';
+import { Video, AVPlaybackStatus } from 'expo-av';
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import { View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 
-const playbackObject = new Audio.Sound();
+import * as FileSystem from 'expo-file-system';
+
+const { width, height } = Dimensions.get('window');
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+
+  const [videoUrl, setVideoUrl] = useState('http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4')
+  const [buttonTitle, setButtonTitle] = useState('Download')
+  const [progressValue, setProgressValue] = useState(0)
+  const [totalSize, setTotalSize] = useState(0)
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <Video
+        source={{ uri: videoUrl }}
+        rate={1.0}
+        volume={1.0}
+        isMuted={false}
+        resizeMode="cover"
+        shouldPlay={true}
+        isLooping={false}
+        useNativeControls
+        style={styles.video}
+      />
     </View>
   );
 }
@@ -34,4 +49,9 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
+  video: {
+    width: '100%',
+    //height: '50%',
+    alignSelf: 'center'
+  }
 });
